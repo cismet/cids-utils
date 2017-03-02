248efc6d39ed84ab150089f069d1bf8322f8394f
p-a-s-c-a-l <pascal@cismet.de>
2017-03-02 09:03:50 +0100

    #7 remove unused declared dependencies and add used undeclared dependencies

--- src/main/java/de/cismet/cids/admin/serverManagement/SimpleWebServer.java
+++ src/main/java/de/cismet/cids/admin/serverManagement/SimpleWebServer.java
@@ -15,8 +15,6 @@ import org.mortbay.jetty.Handler;
 import org.mortbay.jetty.Server;
 import org.mortbay.jetty.handler.*;
 import org.mortbay.jetty.handler.ContextHandlerCollection;
-import org.mortbay.jetty.handler.DefaultHandler;
-import org.mortbay.jetty.handler.HandlerCollection;
 import org.mortbay.jetty.handler.ResourceHandler;
 import org.mortbay.jetty.servlet.Context;
 import org.mortbay.jetty.servlet.ServletHolder;
